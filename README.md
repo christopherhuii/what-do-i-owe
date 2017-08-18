ed0cb7b9c337fa29175a34b41905d69e63e782cc
Christopher Hui <christopherhuii@Christophers-MacBook-Pro.local>
2017-08-18 15:12:24 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# What Do I Owe?
+
+A basic web application that helps friends split bills when eating out
+
+This project was bootstrapped with [Create React App](https://github.com/facebookincubator/create-react-app).
---
+++ src/app.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class WhatDoIOwe extends Component {
+  render() {
+    return (
+      <div className="app">
+          Hello World!
+      </div>
+    );
+  }
+}
+
+export default WhatDoIOwe;
